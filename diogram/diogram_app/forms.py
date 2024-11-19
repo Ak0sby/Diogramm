c70091b913904266de07be31010aa54af4390867
Ak0sby <r.ak0sby@gmail.com>
2024-11-19 16:22:44 +0600

    photo

--- diogram/diogram_app/forms.py
+++ diogram/diogram_app/forms.py
@@ -5,3 +5,5 @@ class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
         fields = ['spravki', 'post_cpgu', 'treb_mil', 'vlitiya_kart', 'aktual', 'post_prekr', 'post_objavl', 'istreb']
+
+
